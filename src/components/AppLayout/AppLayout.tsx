77ddd12b9836fdf8fa142260ef31b03083e0e0b5
umbrellaDuck <cksdn1013@gmail.com>
2023-05-02 10:14:23 +0900

    layout modified

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -8,7 +8,7 @@ const AppLayout = ({ children }: Props) => {
   return (
     <>
       <Header />
-      <main className='pt-[86px] h-full'>{children}</main>
+      <main className='pt-[86px] h-screen'>{children}</main>
     </>
   );
 };
