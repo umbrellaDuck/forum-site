b358a618d1af7689875b04a97594ed59517d6198
umbrellaDuck <cksdn1013@gmail.com>
2023-05-05 17:23:25 +0900

    add validation check each input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,12 +1,53 @@
-interface Props extends React.ComponentPropsWithoutRef<'input'> {}
-const rootClassName = 'rounded-[5px] border-borderGrayColor border';
+import cn from 'clsx';
+import Image from 'next/image';
+import {
+  Control,
+  FieldPath,
+  FieldValues,
+  RegisterOptions,
+  useController,
+} from 'react-hook-form';
+import xCircleIcon from '../../../public/x-circle.svg';
+import checkIcon from '../../../public/check.svg';
 
-const Input = ({ ...rest }: Props) => {
+export interface TControl<T extends FieldValues>
+  extends React.ComponentPropsWithoutRef<'input'> {
+  control: Control<T>;
+  name: FieldPath<T>;
+  rules?: Omit<
+    RegisterOptions<T>,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
+  >;
+}
+const rootClass =
+  'rounded-[5px] border-borderGrayColor border placeholder-placeHolderTextColor placeholder:text-sm h-[42px] px-4 w-full placeholder:text-buttonGrayColor placeholder:font-light';
+
+const Input = <T extends FieldValues>({
+  control,
+  name,
+  rules,
+  className,
+  ...rest
+}: TControl<T>) => {
+  const rootClassName = cn(rootClass, className, {});
+  const {
+    field,
+    fieldState: { invalid },
+  } = useController({ name, rules, control });
+  const val = field.value;
+
   return (
-    <input
-      className='rounded-[5px] border-borderGrayColor border placeholder-placeHolderTextColor placeholder:text-sm h-[42px] px-4 w-full placeholder:text-buttonGrayColor placeholder:font-light'
-      {...rest}
-    />
+    <>
+      <input className={rootClassName} {...rest} {...field} />
+
+      {val && (
+        <Image
+          alt='status-icon'
+          src={invalid ? xCircleIcon : checkIcon}
+          className='absolute right-4 bottom-[14px]'
+        />
+      )}
+    </>
   );
 };
 
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,32 +1,61 @@
 import Button from '../Button.tsx/Button';
 import Input from '../Input/Input';
-import Image from 'next/image';
-import xCircleIcon from '../../../public/x-circle.svg';
-import checkIcon from '../../../public/check.svg';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-let isValid = true;
+const defaultValues = {
+  username: '',
+  email: '',
+  password: '',
+  repeatPassword: '',
+};
 
 const LoginForm = () => {
+  const {
+    control,
+    getValues,
+    formState: { isValid },
+    handleSubmit,
+  } = useForm({
+    defaultValues,
+    mode: 'onChange',
+  });
+
+  type FormValues = {
+    username: string;
+    email: string;
+    password: string;
+    repeatPassword: string;
+  };
+
+  const isExistingEmail = true;
+
+  const onSubmit: SubmitHandler<FormValues> = (data, e) => {
+    console.log('submit');
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1 className='text-2xl font-bold mb-5'>Join Alem Community </h1>
       <p className='leading-6 font-thin mb-5'>
         Get more features and priviliges by joining
-        <div>to the most helpful community</div>
+        <br />
+        to the most helpful community
       </p>
       <div className='gap-4 flex flex-col'>
         <div className='relative'>
-          <label htmlFor='register-id' className='text-base mb-2 inline-block'>
+          <label
+            htmlFor='register-username'
+            className='text-base mb-2 inline-block'
+          >
             Username
           </label>
-          <Input id='register-id' placeholder='Username' />
-          {isValid && (
-            <Image
-              alt='status-icon'
-              src={isValid ? checkIcon : xCircleIcon}
-              className='absolute right-4 bottom-[14px]'
-            />
-          )}
+          <Input
+            id='register-username'
+            placeholder='Username'
+            name='username'
+            rules={{ minLength: 2, maxLength: 12, required: true }}
+            control={control}
+          />
         </div>
         <div className='relative'>
           <label
@@ -35,14 +64,15 @@ const LoginForm = () => {
           >
             Email
           </label>
-          <Input id='register-email' placeholder='Email' />
-          {isValid && (
-            <Image
-              alt='status-icon'
-              src={isValid ? checkIcon : xCircleIcon}
-              className='absolute right-4 bottom-[14px]'
-            />
-          )}
+          <Input
+            id='register-email'
+            placeholder='Email'
+            name='email'
+            rules={{
+              required: true,
+            }}
+            control={control}
+          />
         </div>
         <div className='relative'>
           <label
@@ -51,43 +81,43 @@ const LoginForm = () => {
           >
             Password
           </label>
-          <Input id='register-password' placeholder='Password' />
-          {isValid && (
-            <Image
-              alt='status-icon'
-              src={isValid ? checkIcon : xCircleIcon}
-              className='absolute right-4 bottom-[14px]'
-            />
-          )}
+          <Input
+            id='register-password'
+            placeholder='Password'
+            name='password'
+            rules={{ minLength: 4, maxLength: 16, required: true }}
+            control={control}
+          />
         </div>
         <div className='relative'>
           <label
-            htmlFor='register-repeat-pasword'
+            htmlFor='register-repeat-password'
             className='text-base mb-2 inline-block'
           >
             Repeat pasword
           </label>
-          <Input id='register-repeat-pasword' placeholder='Repeat pasword' />
-          {isValid && (
-            <Image
-              alt='status-icon'
-              src={isValid ? checkIcon : xCircleIcon}
-              className='absolute right-4 bottom-[14px]'
-            />
-          )}
+          <Input
+            id='register-repeat-paswsord'
+            placeholder='Repeat pasword'
+            name='repeatPassword'
+            rules={{
+              validate: (val: string) => val == getValues('password'),
+            }}
+            control={control}
+          />
         </div>
       </div>
-      {
+      {isExistingEmail && (
         <p className='text-[#FF0000] text-xs mt-6 mb-6 font-bold ml-4'>
           Email already registered!
         </p>
-      }
+      )}
       <Button
         color='orange'
         size='md'
         className='w-full'
         type='submit'
-        disabled
+        disabled={!isValid}
       >
         <span className='text-xs font-black'>REGISTER</span>
       </Button>
